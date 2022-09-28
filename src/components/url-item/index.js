4431a986d480bdb067bd7eefc26fd974e95f7e79
ShizzaHo <chekan898@gmail.com>
2022-09-28 14:21:38 +0500

    Transferring the pick Text Color function to a separate utility

--- src/components/url-item/index.js
+++ src/components/url-item/index.js
@@ -12,6 +12,7 @@ import {
   IonIcon,
 } from '@ionic/react';
 import { star, trash, pencil } from 'ionicons/icons';
+import { pickTextColor } from './../../utils/pickTextColor/index';
 
 function LinkItem({
   title,
@@ -118,21 +119,5 @@ LinkItem.defaultProps = {
   showDeleteButton: false,
   swipeIcons: false,
 };
-
-function pickTextColor(bgColor, lightColor, darkColor) {
-  const color = bgColor.charAt(0) === '#' ? bgColor.substring(1, 7) : bgColor;
-  const r = parseInt(color.substring(0, 2), 16); // hexToR
-  const g = parseInt(color.substring(2, 4), 16); // hexToG
-  const b = parseInt(color.substring(4, 6), 16); // hexToB
-  const uicolors = [r / 255, g / 255, b / 255];
-  const c = uicolors.map((col) => {
-    if (col <= 0.03928) {
-      return col / 12.92;
-    }
-    return Math.pow((col + 0.055) / 1.055, 2.4);
-  });
-  const L = 0.2126 * c[0] + 0.7152 * c[1] + 0.0722 * c[2];
-  return L > 0.179 ? darkColor : lightColor;
-}
 
 export default LinkItem;
